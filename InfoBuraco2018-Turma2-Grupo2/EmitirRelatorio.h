a9cae91ef7501fb330c153e122c5a6875da4bc2e
Fernando Monteiro <fernando.vicente.monteiro@usp.br>
2018-05-09 23:08:56 -0300

    Adição da Tabela 10 (Emitir Relatório)

---
+++ InfoBuraco2018-Turma2-Grupo2/EmitirRelatorio.h
@@ -0,0 +1,156 @@
+#pragma once
+
+namespace InfoBuraco2018Turma2Grupo2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Sumário para EmitirRelatorio
+	/// </summary>
+	public ref class EmitirRelatorio : public System::Windows::Forms::Form
+	{
+	public:
+		EmitirRelatorio(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Adicione o código do construtor aqui
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		~EmitirRelatorio()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+	private: System::Windows::Forms::Label^  label3;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(150, 149);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Voltar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 52);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(91, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Tipo de Relatório:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(12, 99);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(25, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"OS:";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(44, 149);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Gerar";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Físico", L"Físico-Financeiro" });
+			this->comboBox1->Location = System::Drawing::Point(124, 49);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 4;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &EmitirRelatorio::comboBox1_SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Location = System::Drawing::Point(124, 96);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(121, 21);
+			this->comboBox2->TabIndex = 5;
+			this->comboBox2->SelectedIndexChanged += gcnew System::EventHandler(this, &EmitirRelatorio::comboBox2_SelectedIndexChanged);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(79, 18);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(102, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Emição de Relatório";
+			// 
+			// EmitirRelatorio
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(257, 197);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Name = L"EmitirRelatorio";
+			this->Text = L"EmitirRelatorio";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void comboBox2_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
+}
