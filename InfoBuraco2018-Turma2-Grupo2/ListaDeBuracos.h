44edefed02ec1bbfd2c822ddf913ffe4b155053c
Fernando Monteiro <fernando.vicente.monteiro@usp.br>
2018-05-09 22:06:52 -0300

    Adição das Telas Formulação De Plano de Saída (4) e Lista De Buracos (2)

---
+++ InfoBuraco2018-Turma2-Grupo2/ListaDeBuracos.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace InfoBuraco2018Turma2Grupo2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Sumário para ListaDeBuracos
+	/// </summary>
+	public ref class ListaDeBuracos : public System::Windows::Forms::Form
+	{
+	public:
+		ListaDeBuracos(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Adicione o código do construtor aqui
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		~ListaDeBuracos()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"ListaDeBuracos";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
